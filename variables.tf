1c65275267cbb306fe2a5f7e5a8a6c900ca3d70b
elimarhenrique <elimar.henrique@yahoo.com.br>
2022-10-19 20:23:24 -0300

    Implementando cluster EKS

--- variables.tf
+++ variables.tf
@@ -5,3 +5,21 @@ variable "cluster_name" {
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "k8s_version" {
+  default = "1.23"
+}
+
+variable "nodes_instances_sizes" {
+  default = [
+    "t3.large"
+  ]
+}
+
+variable "auto_scale_options" {
+  default = {
+     min     = 2
+     max     = 10
+     desired = 2
+  }
+}
